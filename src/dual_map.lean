3bbcd7c77c461ebfd56c786380ba3bb0212a7f99
Kexing <kexing.ying19@imperial.ac.uk>
2021-03-22 00:54:26 +0100

    dual_annihilator lemma

--- src/dual_map.lean
+++ src/dual_map.lean
@@ -77,3 +77,24 @@ end
 end finite_dimensional
 
 end linear_map
+
+namespace submodule
+
+variables {U V : submodule R M₁}
+
+lemma dual_annihilator_sup_eq_inf_dual_annihilator :
+  (U ⊔ V).dual_annihilator = U.dual_annihilator ⊓ V.dual_annihilator :=
+begin
+  ext φ,
+  rw [mem_inf, mem_dual_annihilator, mem_dual_annihilator, mem_dual_annihilator],
+  split; intro h,
+  { refine ⟨_, _⟩;
+    intros x hx,
+    refine h x (mem_sup.2 ⟨x, hx, 0, zero_mem _, add_zero _⟩),
+    refine h x (mem_sup.2 ⟨0, zero_mem _, x, hx, zero_add _⟩) },
+  { simp_rw mem_sup,
+    rintro _ ⟨x, hx, y, hy, rfl⟩,
+    rw [linear_map.map_add, h.1 _ hx, h.2 _ hy, add_zero] }
+end
+
+end submodule
